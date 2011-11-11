8314d99e9c822f02b4a83651e5761254ca794dcd
Adrian L Lange <adrianlund@gmail.com>
2011-11-11 18:28:03 +0100

    Remove server and use internal directory for windows support

--- ext/dotjs.js
+++ ext/dotjs.js
@@ -1,10 +1,19 @@
 $.ajax({
-  url: 'http://localhost:3131/'+window.location.host.replace('www.','')+'.js',
-  dataType: 'text',
-  success: function(d){
-    $(function(){ eval(d) })
-  },
-  error: function(){
-    console.log('no dotjs server found at localhost:3131')
-  }
+	url: chrome.extension.getURL('scripts/' + window.location.host.replace('www.', '') + '.js'),
+	dataType: 'text',
+	success: function(data){
+		$(function(){
+			eval(data);
+		})
+	}
+});
+
+$.ajax({
+	url: chrome.extension.getURL('scripts/default.js'),
+	dataType: 'text',
+	done: function(data){
+		$(function(){
+			eval(data);
+		}
+	}
 })
