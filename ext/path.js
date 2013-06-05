a5b09213065bf1347a4acedf0d84c8613146a3e8
Adrian L Lange <adrianlund@gmail.com>
2013-06-05 21:01:27 +0200

    Autofocus and select the link

--- ext/path.js
+++ ext/path.js
@@ -3,6 +3,9 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'manifest.json');
 xhr.onload = function(){
 	var manifest = JSON.parse(xhr.responseText);
-	document.getElementById('link').value = 'C:\\Users\\%USERNAME%\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\' + chrome.i18n.getMessage('@@extension_id') + '\\' + manifest.version + '_0\\scripts';
+	var link = document.getElementById('link');
+	link.value = 'C:\\Users\\%USERNAME%\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Extensions\\' + chrome.i18n.getMessage('@@extension_id') + '\\' + manifest.version + '_0\\scripts';
+	link.focus();
+	link.select();
 }
 xhr.send(null);
