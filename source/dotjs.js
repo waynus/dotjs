d9c9aae3cf67bfd873cc7c79cff0a41905573179
p3lim <adrianlund@gmail.com>
2013-07-17 17:34:53 +0200

    Add support for injecting css from /styles

--- source/dotjs.js
+++ source/dotjs.js
@@ -1,5 +1,18 @@
+var hostname = location.hostname.replace(/^www\./, '')
+
 $.ajax({
-	url: chrome.extension.getURL('scripts/' + location.hostname.replace(/^www\./, '') + '.js'),
+	url: chrome.extension.getURL('styles/' + hostname + '.css'),
+	dataType: 'text',
+	success: function(data){
+		var stylesheet = document.createElement('style');
+		stylesheet.type = 'text/css';
+		stylesheet.innerHTML = data;
+		document.body.appendChild(stylesheet);
+	}
+})
+
+$.ajax({
+	url: chrome.extension.getURL('scripts/' + hostname + '.js'),
 	dataType: 'text',
 	success: function(data){
 		eval(data);
