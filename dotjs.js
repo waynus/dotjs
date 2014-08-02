eef93ee29ecec1f150dc8af0e9277709a39daaed
Adrian L Lange <adrianlund@gmail.com>
2014-08-02 07:18:53 +0200

    Use appendChild instead of insertBefore for a small perfomance boost
    - Fixes #11

--- dotjs.js
+++ dotjs.js
@@ -3,17 +3,17 @@ var hostname = location.hostname.replace(/^www\./, '');
 var style = document.createElement('link');
 style.rel = 'stylesheet';
 style.href = chrome.extension.getURL('styles/' + hostname + '.css');
-document.documentElement.insertBefore(style);
+document.documentElement.appendChild(style);
 
 var defaultStyle = document.createElement('link');
 defaultStyle.rel = 'stylesheet';
 defaultStyle.href = chrome.extension.getURL('styles/default.css');
-document.documentElement.insertBefore(defaultStyle);
+document.documentElement.appendChild(defaultStyle);
 
 var script = document.createElement('script');
 script.src = chrome.extension.getURL('scripts/' + hostname + '.js');
-document.documentElement.insertBefore(script);
+document.documentElement.appendChild(script);
 
 var defaultScript = document.createElement('script');
 defaultScript.src = chrome.extension.getURL('scripts/default.js');
-document.documentElement.insertBefore(defaultScript);
+document.documentElement.appendChild(defaultScript);
